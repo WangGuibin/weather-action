b7c14b1711a63f411011193a1c407af9820191b3
Wangguibin <864562082@qq.com>
2021-12-11 15:40:39 +0800

    fix

--- py.py
+++ py.py
@@ -5,15 +5,12 @@ titleArr = soup.select('.u')
 #标题
 headerTitle = titleArr[0].get_text()
 formatText = headerTitle + '\n'
-#天气 github action 容器没有ip的 天气不好弄了 算了算了 就这吧
-# weather = soup.select('.weather')[0].string
-# formatText += weather + '\n'
 
 #新闻
 newsStr = ""
 newsElement = soup.select('.news-wrap > .line')
 for div in newsElement:
-	news = '![' + div.get_text() + '](' + div.a.get('href') + ')' + '\n'
+	news = div.get_text() + '\n'
 	newsStr += news
 formatText += newsStr + '\n'
 
@@ -35,7 +32,7 @@ progress = '时间进度条: ' + soup.select('.progress-bar')[0].get_text()
 progress_text = soup.select('.line')[-1].get_text()
 formatText += progress + '\n'
 formatText += progress_text + '\n'
-filename = 'result.md'
+filename = 'result.txt'
 with open (filename,'w') as file:
     file.write(formatText)   
 
