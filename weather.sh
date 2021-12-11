4b460a523bab307e414596ab01e8810e6ffb63c7
Wangguibin <864562082@qq.com>
2021-12-11 13:15:06 +0800

    upload~

--- weather.sh
+++ weather.sh
@@ -2,16 +2,6 @@
 
 set -eux
 
-CITY=Shenzhen
-LANGUAGE="zh-CN"
-UNIT=m
-UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"
-
-curl \
-  -H "Accept-Language: $LANGUAGE" \
-  -H "User-Agent: $UA" \
-  -o result.html \
-  wttr.in/$CITY?format=4\&$UNIT
-
+curl https://news.topurl.cn > result.html
 
 
